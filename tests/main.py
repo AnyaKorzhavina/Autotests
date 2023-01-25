65231a17e91174404a8e7e85c20e25ef5139f0da
Anna Korzhavina <121830307+AnyaKorzhavina@users.noreply.github.com>
2023-01-25 18:55:50 +0300

    Add files via upload

---
+++ tests/main.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+token = 'ad36816d99b9be4ebcbd247246f8bbd4'
+response_post = requests.post('https://pokemonbattle.me:5000/pokemons', headers={'trainer_token': token},json={
+    "name": "Джеки",
+    "photo": "https://static.wikia.nocookie.net/pokemon/images/2/21/001Bulbasaur.png"
+})
+
+print(response_post.text)
+
+response_put = requests.put('https://pokemonbattle.me:5000/pokemons', headers={'trainer_token': token},json={
+    "pokemon_id": 3348,
+    "name": "German",
+    "photo": ""
+})
+print(response_put.text)
+
+response_post_pokeball = requests.post('https://pokemonbattle.me:5000/trainers/add_pokeball', headers={'trainer_token': token},json={
+   "pokemon_id": "3350"
+})
+print(response_post_pokeball.text)
